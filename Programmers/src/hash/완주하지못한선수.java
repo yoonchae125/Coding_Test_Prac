1b963d65a886c1dd5e6d03e9c05ad99bd3c8d378
chaeyoon <yoonchae125@naver.com>
2020-01-04 19:17:31 +0900

    .

---
+++ Programmers/src/hash/완주하지못한선수.java
@@ -0,0 +1,29 @@
+package hash;
+
+public class 완주하지못한선수 {
+
+	public static String solution(String[] participant, String[] completion) {
+        String answer = "";
+        return answer;
+    }
+	public static void main(String[] args) {
+		
+		// leo
+		String[] participant1 = {"leo", "kiki", "eden"};
+		String[] completion1 = {"eden", "kiki"};
+		// vinko
+		String[] participant2 = {"marina", "josipa", "nikola", "vinko", "filipa"};
+		String[] completion2 = {"josipa", "filipa", "marina", "nikola"};
+		// mislav
+		String[] participant3 = {"mislav", "stanko", "mislav", "ana"};
+		String[] completion3 = {"stanko", "ana", "mislav"};
+		
+		// leo
+		solution(participant1,completion1);
+		// vinko
+		solution(participant2,completion2);
+		// mislav
+		solution(participant3,completion3);
+	}
+
+}
