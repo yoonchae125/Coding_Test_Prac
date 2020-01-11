03d3a72ce90dd4ea49640cac013796f1c8b3ea97
chaeyoon <yoonchae125@naver.com>
2020-01-11 20:03:05 +0900

    ㅇ

--- Programmers/src/stack_queue/탑.java
+++ Programmers/src/stack_queue/탑.java
@@ -7,7 +7,7 @@ public class 탑 {
         for(int i=heights.length-1;i>0;i--) {
         	for(int j=i-1;j>=0;j--) {
         		if(heights[i]<heights[j]) {
-        			answer[i]=j;
+        			answer[i]=j+1;
         			break;
         		}
         	}
